c69a677ed2169375b66a1cd79e85c9ce943af0e2
lorevin <lorevin@163.com>
2023-07-07 22:42:01 +0800

    流的随机访问

---
+++ liudesuijifangwen.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <fstream>
+using std::cout;using std::endl;using std::cin;using std::string;using std::vector;
+int main(){
+  //创建文件流对象
+  std::ifstream instream;
+  string 
+  return 0;
+}
